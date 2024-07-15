3dcb7fadd0926a3665ccd946517be3c9957dd926
yram-nna12 <202311557@fit.edu.ph>
2024-07-15 23:00:21 +0800

    fix (project-page): added and updated button

--- pages/feedback/assets/js/script.js
+++ pages/feedback/assets/js/script.js
@@ -1,3 +1,25 @@
-document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-    alert('Thank you for your feedback!');
-});
+ document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+            event.preventDefault(); 
+            const formData = new FormData(event.target);
+            const data = {};
+            formData.forEach((value, key) => {
+                data[key] = value;
+            });
+
+            fetch(event.target.action, {
+                method: 'POST',
+                mode: 'no-cors', 
+                body: JSON.stringify(data),
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert('Thank You for your feedback!');
+                } else {
+                    alert('Error submitting feedback. Please try again.');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Error submitting feedback. Please try again.');
+            });
+        });
