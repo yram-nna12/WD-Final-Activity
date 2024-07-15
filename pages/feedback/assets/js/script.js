ecf94092c30210b9095a1ec8594a175725d8880c
yram-nna12 <202311557@fit.edu.ph>
2024-07-16 07:00:24 +0800

    fix (feedback-page): updated google sheet link

--- pages/feedback/assets/js/script.js
+++ pages/feedback/assets/js/script.js
@@ -1,25 +1,23 @@
- document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-            event.preventDefault(); 
-            const formData = new FormData(event.target);
-            const data = {};
-            formData.forEach((value, key) => {
-                data[key] = value;
-            });
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-            fetch(event.target.action, {
-                method: 'POST',
-                mode: 'no-cors', 
-                body: JSON.stringify(data),
-            })
-            .then(response => {
-                if (response.ok) {
-                    alert('Thank You for your feedback!');
-                } else {
-                    alert('Error submitting feedback. Please try again.');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('Error submitting feedback. Please try again.');
-            });
-        });
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    fetch('https://script.google.com/macros/s/AKfycbwP-H5272wkDMeEYphPIM_fZeKUQGJrpl4k03H8nBE5LBoeIJvpln1xoEschnFoUf0oTA/exec', {
+        method: 'POST',
+        mode: 'no-cors',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        alert('Thank You for your feedback!');
+        event.target.reset(); 
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error submitting feedback. Please try again.');
+    });
+});
